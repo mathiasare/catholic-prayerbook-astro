cd70c8a0aca28047ff0622403ab24e7add7cb773
mathiasare <mathias.are@ridango.com>
2022-08-26 15:56:56 +0300

    Added navigation functionality to prayers with accordion menu

---
+++ app/src/components/Accordion.tsx
@@ -0,0 +1,39 @@
+import {Root ,Item, Content, Trigger, Header} from '@radix-ui/react-accordion'
+import { ChevronDownIcon } from '@radix-ui/react-icons';
+import s from '@styles/Accordion.module.css'
+import type { FC } from 'react';
+
+interface AccordionProps {
+    content: { [index: string]: string[]  }
+  }
+
+export const Accordion: React.FC<AccordionProps> = (props) => {
+    const { content } = props;
+    const menuItems = Object.keys(content).map((category) => {
+          const contents = content[category].map((prayer) => {
+            const path = `${category}/${prayer}/`
+            return <li key={prayer}><a href={path} className="hover:text-red-800 hover:font-bold">{prayer}</a></li>
+          });
+          return (
+            <Item value={category} className={s.Item} key={category}>
+              <Header className={s.Header}>
+                <Trigger className={s.Trigger}>
+                  <span>{category}</span>
+                  <ChevronDownIcon aria-hidden className={s.Icon} />
+                </Trigger>
+              </Header>
+              <Content className={s.Content}>
+                <ul>
+                  {contents}
+                </ul>
+              </Content>
+            </Item>
+          )
+    })
+    return (
+        <>
+        <Root type="multiple" className={s.Container}>
+          {menuItems}
+        </Root>
+      </>
+  );}
--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+const plugin = require('tailwindcss/plugin')
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
-}
+	plugins: [
+		plugin(function ({ addVariant, e }) {
+			addVariant('data-state-open', ({ modifySelectors, separator }) => {
+				modifySelectors(({ className }) => {
+					return `.${e(`data-state-open${separator}${className}`)}[data-state='open']`
+				}
+			)})
+		  })
+		]
+});
